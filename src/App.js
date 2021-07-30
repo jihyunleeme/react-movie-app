ef902a57316c4bdde8eca2b40f54d67818afc65b
jihyunlee <jihyunlee.me@gmail.com>
2021-07-30 16:57:02 +0900

    #3.3 Planning the Movie Component - componentDidMount과 setState 사용하기

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import propTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
 
-  add = () => { this.setState(current => ({ count: ++current.count})) }
-  minus = () => { this.setState(current => ({ count: --current.count })) }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({isLoading: false, book: true});
+    }, 6000);
+  }
 
   render() {
-    return (
-      <>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.minus}>minus</button>
-      </>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>
   }
 }
 
