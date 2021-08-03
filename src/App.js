cfd1bacd3fb39e3518adaeb5f879b16eb1537f7f
jihyunlee <jihyunlee.me@gmail.com>
2021-08-03 11:41:41 +0900

    #6.0 Building the Navigation - 라우터 기본

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
-import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
+import React from 'react'
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  componentDidMount() {
-    console.log("componentDidMount 작동")
-    this.getMovies();
-  }
-
-  getMovies = async() => {
-    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
-    // const temp = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");  // line 11 과 동일
-    console.log('movies = ', movies);
-    // console.log('temp = ', temp); 
-    this.setState({ movies, isLoading: false })
-  }
-
-  render() {
-    const { isLoading , movies } = this.state;
-    return <div>{isLoading ? (
-      <div className="loader">
-        <span className="loader__text">Loading...</span>
-      </div>
-    ) : (
-    <div className="movies">
-      {movies.map(movie => {
-      console.log(movie);
-      return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}/>
-      })}
-    </div>
-    )}</div>
-  }
+function App() {
+  return <HashRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" exact component={About} />
+    </HashRouter>
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+function About() {
+    return <span> About About About About About About About About About About About About About About About About About About</span>
+}
+export default About;
